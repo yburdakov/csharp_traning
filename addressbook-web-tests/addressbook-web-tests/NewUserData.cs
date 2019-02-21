e8455925b7d61c1f7e1f13785c15db115499dc21
yburdakov <j_bur@mail.ru>
2019-02-22 00:15:28 +0300

    Task #4 completed successfully

---
+++ addressbook-web-tests/addressbook-web-tests/NewUserData.cs
@@ -0,0 +1,332 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressbookTests
+{
+    class NewUserData
+    {
+        private string firstname = "";
+        private string middlename = "";
+        private string lastname = "";
+        private string nickname = "";
+        private string title = "";
+        private string company = "";
+        private string address = "";
+        private string home = "";
+        private string mobile = "";
+        private string work = "";
+        private string fax = "";
+        private string email = "";
+        private string email2 = "";
+        private string email3 = "";
+        private string homepage = "";
+        private string address2 = "";
+        private string phone2 = "";
+        private string notes = "";
+        private string byear = "";
+        private string bday = "";
+        private string bmonth = "";
+        private string ayear = "";
+        private string aday = "";
+        private string amonth = "";
+
+        public NewUserData(string firstname, string middlename, string lastname)
+        {
+            this.firstname = firstname;
+            this.middlename = middlename;
+            this.lastname = lastname;
+        }
+        public string Firstname
+        {
+            get
+            {
+                return firstname;
+            }
+            set
+            {
+                firstname = value;
+            }
+        }
+
+        public string Middlename
+        {
+            get
+            {
+                return middlename;
+            }
+            set
+            {
+                middlename = value;
+            }
+        }
+
+
+        public string Lastname
+        {
+            get
+            {
+                return lastname;
+            }
+            set
+            {
+                lastname = value;
+            }
+        }
+
+        public string Nickname
+        {
+            get
+            {
+                return nickname;
+            }
+            set
+            {
+                nickname = value;
+            }
+        }
+
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+            set
+            {
+                title = value;
+            }
+        }
+
+        public string Company
+        {
+            get
+            {
+                return company;
+            }
+            set
+            {
+                company = value;
+            }
+        }
+
+        public string Address
+        {
+            get
+            {
+                return address;
+            }
+            set
+            {
+                address = value;
+            }
+        }
+
+        public string Home
+        {
+            get
+            {
+                return home;
+            }
+            set
+            {
+                home = value;
+            }
+        }
+
+        public string Mobile
+        {
+            get
+            {
+                return mobile;
+            }
+            set
+            {
+                mobile = value;
+            }
+        }
+
+        public string Work
+        {
+            get
+            {
+                return work;
+            }
+            set
+            {
+                work = value;
+            }
+        }
+
+        public string Fax
+        {
+            get
+            {
+                return fax;
+            }
+            set
+            {
+                fax = value;
+            }
+        }
+
+
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+            set
+            {
+                email = value;
+            }
+        }
+
+        public string Email2
+        {
+            get
+            {
+                return email2;
+            }
+            set
+            {
+                email2 = value;
+            }
+        }
+
+        public string Email3
+        {
+            get
+            {
+                return email3;
+            }
+            set
+            {
+                email3 = value;
+            }
+        }
+
+        public string Homepage
+        {
+            get
+            {
+                return homepage;
+            }
+            set
+            {
+                homepage = value;
+            }
+        }
+
+        public string Address2
+        {
+            get
+            {
+                return address2;
+            }
+            set
+            {
+                address2 = value;
+            }
+        }
+
+        public string Phone2
+        {
+            get
+            {
+                return phone2;
+            }
+            set
+            {
+                phone2 = value;
+            }
+        }
+
+        public string Notes
+        {
+            get
+            {
+                return notes;
+            }
+            set
+            {
+                notes = value;
+            }
+        }
+        
+        public string Byear
+        {
+            get
+            {
+                return byear;
+            }
+            set
+            {
+                byear = value;
+            }
+        }
+
+        public string Bday
+        {
+            get
+            {
+                return bday;
+            }
+            set
+            {
+                bday = value;
+            }
+        }
+
+        public string Bmonth
+        {
+            get
+            {
+                return bmonth;
+            }
+            set
+            {
+                bmonth = value;
+            }
+        }
+
+        public string Ayear
+        {
+            get
+            {
+                return ayear;
+            }
+            set
+            {
+                ayear = value;
+            }
+        }
+        public string Aday
+        {
+            get
+            {
+                return aday;
+            }
+            set
+            {
+                aday = value;
+            }
+        }
+
+        public string Amonth
+        {
+            get
+            {
+                return amonth;
+            }
+            set
+            {
+                amonth = value;
+            }
+        }
+
+    }
+}
--- addressbook-web-tests/addressbook-web-tests/UserCreationTest.cs
+++ addressbook-web-tests/addressbook-web-tests/UserCreationTest.cs
@@ -44,61 +44,100 @@ namespace WebAddressbookTests
         {
             OpenHomePage();
             Login(new AccountData("admin", "secret"));
-            driver.FindElement(By.Name("MainForm")).Click();
-            driver.FindElement(By.LinkText("add new")).Click();
-            driver.FindElement(By.Name("firstname")).Click();
+            InitNewUserCreation();
+            NewUserData newuser = new NewUserData("AAA", "BBB", "CCC");
+                newuser.Nickname = "ddd";
+                newuser.Title = "ddd";
+                newuser.Company = "ddd";
+                newuser.Address = "ddd";
+                newuser.Home = "ddd";
+                newuser.Work = "ddd";
+                newuser.Email = "ddd";
+                newuser.Email2 = "ddd";
+                newuser.Email3 = "ddd";
+                newuser.Homepage = "ddd";
+                newuser.Address2 = "ddd";
+                newuser.Notes = "ddd";
+            //please use correct months names for these below
+            //cause I don't know how to manage dropdowns yet)))
+                newuser.Bmonth = "November";
+                newuser.Amonth = "July";
+            //please use integer for these below, 
+            //cause I don't know how to manage formats 
+            //and how to consider fields validation)))
+                newuser.Fax = "123";
+                newuser.Mobile = "234";
+                newuser.Phone2 = "345";
+                newuser.Byear = "1991";
+                newuser.Bday = "1"; // when using <0 or >[max for a mo] step must fail
+                newuser.Ayear = "2016";
+                newuser.Aday = "15"; // when using <0 or >[max for a mo] step must fail
+            FillNewUserForm(newuser);
+            SubmitNewUserCreation();
+            Logout();
+        }
+
+        private void SubmitNewUserCreation()
+        {
+            driver.FindElement(By.Name("submit")).Click();
+        }
+
+        private void FillNewUserForm(NewUserData newuser)
+        {
             driver.FindElement(By.Name("firstname")).Clear();
-            driver.FindElement(By.Name("firstname")).SendKeys("aaa");
+            driver.FindElement(By.Name("firstname")).SendKeys(newuser.Firstname);
             driver.FindElement(By.Name("middlename")).Clear();
-            driver.FindElement(By.Name("middlename")).SendKeys("bbb");
+            driver.FindElement(By.Name("middlename")).SendKeys(newuser.Middlename);
             driver.FindElement(By.Name("lastname")).Clear();
-            driver.FindElement(By.Name("lastname")).SendKeys("cccName");
+            driver.FindElement(By.Name("lastname")).SendKeys(newuser.Lastname);
             driver.FindElement(By.Name("nickname")).Clear();
-            driver.FindElement(By.Name("nickname")).SendKeys("nnn");
+            driver.FindElement(By.Name("nickname")).SendKeys(newuser.Nickname);
             driver.FindElement(By.Name("title")).Clear();
-            driver.FindElement(By.Name("title")).SendKeys("ttt");
+            driver.FindElement(By.Name("title")).SendKeys(newuser.Title);
             driver.FindElement(By.Name("company")).Clear();
-            driver.FindElement(By.Name("company")).SendKeys("qqq");
+            driver.FindElement(By.Name("company")).SendKeys(newuser.Company);
             driver.FindElement(By.Name("address")).Clear();
-            driver.FindElement(By.Name("address")).SendKeys("eee");
+            driver.FindElement(By.Name("address")).SendKeys(newuser.Address);
             driver.FindElement(By.Name("home")).Clear();
-            driver.FindElement(By.Name("home")).SendKeys("rrr");
+            driver.FindElement(By.Name("home")).SendKeys(newuser.Home);
             driver.FindElement(By.Name("mobile")).Clear();
-            driver.FindElement(By.Name("mobile")).SendKeys("vvv");
+            driver.FindElement(By.Name("mobile")).SendKeys(newuser.Mobile);
             driver.FindElement(By.Name("work")).Clear();
-            driver.FindElement(By.Name("work")).SendKeys("www");
+            driver.FindElement(By.Name("work")).SendKeys(newuser.Work);
             driver.FindElement(By.Name("fax")).Clear();
-            driver.FindElement(By.Name("fax")).SendKeys("123");
+            driver.FindElement(By.Name("fax")).SendKeys(newuser.Fax);
             driver.FindElement(By.Name("email")).Clear();
-            driver.FindElement(By.Name("email")).SendKeys("qwe");
+            driver.FindElement(By.Name("email")).SendKeys(newuser.Email);
             driver.FindElement(By.Name("email2")).Clear();
-            driver.FindElement(By.Name("email2")).SendKeys("asd");
+            driver.FindElement(By.Name("email2")).SendKeys(newuser.Email2);
             driver.FindElement(By.Name("email3")).Clear();
-            driver.FindElement(By.Name("email3")).SendKeys("zxc");
+            driver.FindElement(By.Name("email3")).SendKeys(newuser.Email3);
             driver.FindElement(By.Name("homepage")).Clear();
-            driver.FindElement(By.Name("homepage")).SendKeys("asd");
-            new SelectElement(driver.FindElement(By.Name("bday"))).SelectByText("1");
-            new SelectElement(driver.FindElement(By.Name("bmonth"))).SelectByText("May");
-            driver.FindElement(By.XPath("//option[@value='May']")).Click();
-            driver.FindElement(By.Name("byear")).Clear();
-            driver.FindElement(By.Name("byear")).SendKeys("1991");
-            new SelectElement(driver.FindElement(By.Name("aday"))).SelectByText("12");
-            driver.FindElement(By.XPath("(//option[@value='12'])[2]")).Click();
-            new SelectElement(driver.FindElement(By.Name("amonth"))).SelectByText("December");
-            driver.FindElement(By.XPath("(//option[@value='December'])[2]")).Click();
-            driver.FindElement(By.Name("ayear")).Clear();
-            driver.FindElement(By.Name("ayear")).SendKeys("2015");
-            driver.FindElement(By.Name("theform")).Click();
-            driver.FindElement(By.XPath("(//option[@value='8'])[3]")).Click();
-            driver.FindElement(By.Name("address2")).Click();
+            driver.FindElement(By.Name("homepage")).SendKeys(newuser.Homepage);
             driver.FindElement(By.Name("address2")).Clear();
-            driver.FindElement(By.Name("address2")).SendKeys("zxc");
+            driver.FindElement(By.Name("address2")).SendKeys(newuser.Address2);
             driver.FindElement(By.Name("phone2")).Clear();
-            driver.FindElement(By.Name("phone2")).SendKeys("zxc");
+            driver.FindElement(By.Name("phone2")).SendKeys(newuser.Phone2);
             driver.FindElement(By.Name("notes")).Clear();
-            driver.FindElement(By.Name("notes")).SendKeys("asd");
-            driver.FindElement(By.XPath("//input[21]")).Click();
+            driver.FindElement(By.Name("notes")).SendKeys(newuser.Notes);
+            driver.FindElement(By.Name("byear")).Clear();
+            driver.FindElement(By.Name("byear")).SendKeys(newuser.Byear);
+            driver.FindElement(By.Name("ayear")).Clear();
+            driver.FindElement(By.Name("ayear")).SendKeys(newuser.Ayear);
+            new SelectElement(driver.FindElement(By.Name("bday"))).SelectByText(newuser.Bday);
+            new SelectElement(driver.FindElement(By.Name("bmonth"))).SelectByText(newuser.Bmonth);
+            new SelectElement(driver.FindElement(By.Name("aday"))).SelectByText(newuser.Aday);
+            new SelectElement(driver.FindElement(By.Name("amonth"))).SelectByText(newuser.Amonth);
+        }
+
+        private void Logout()
+        {
             driver.FindElement(By.LinkText("Logout")).Click();
+        }
+
+        private void InitNewUserCreation()
+        {
+            driver.FindElement(By.LinkText("add new")).Click();
         }
 
         private void Login(AccountData account)
